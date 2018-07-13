44cd8f505cd7963a15af7bce73d20a9f6daad6c1
Victor Palacios <victor@sixtop.net>
2018-07-13 12:47:02 -0500

    Correct led configuration for Arria 10 GX

--- etc/counter_Arria10GX/src/counter.sv
+++ etc/counter_Arria10GX/src/counter.sv
@@ -5,10 +5,10 @@ module counter (
   output  logic [15:0] leds
 );
 
-parameter BW = 24;
+parameter BW = 37;
 logic [BW-1:0] counter_flops;
 
-always_comb leds = counter_flops[BW-1:8];
+always_comb leds = ~counter_flops[BW-1:BW-16];
 
 always_ff @ (posedge clk or negedge reset_b) begin
    if (~reset_b) 
